3176d7f006876234465100f012f0f8a801847a27
Ruud Schouten <ruudschouten@users.noreply.github.com>
2017-12-18 09:45:14 +0100

    Create Logger.java

---
+++ Logger.java
@@ -0,0 +1,27 @@
+public class Logger {
+
+    private java.util.logging.Logger logger;
+
+    public Logger(String name, Level fileLogging, Level commandLogging) {
+        setupLogger(name, fileLogging, commandLogging);
+    }
+
+    public void setupLogger(String name, Level fileLogging, Level commandLogging) {
+        try {
+            String timeStamp = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss").format(Calendar.getInstance().getTime());
+            FileHandler fh = new FileHandler(String.format("logs/%s-%s.log", name, timeStamp));
+            fh.setLevel(fileLogging);
+            ConsoleHandler ch = new ConsoleHandler();
+            ch.setLevel(commandLogging);
+            logger = java.util.logging.Logger.getLogger(name);
+            logger.addHandler(fh);
+            logger.setLevel(Level.ALL);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    public void log(Level level, String message){
+        logger.log(level, message);
+    }
+}
